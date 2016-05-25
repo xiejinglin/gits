a2ff8c81931f3d1723de9c5ab9896df8429cf035
xiejinglin <xiejinglin@DESKTOP-O6E9JP4>
2016-05-25 16:18:00 +0800

    完成了配置文件，pom

---
+++ Contact/src/main/java/com/cmbc/po/Contact.java
@@ -0,0 +1,65 @@
+package com.cmbc.po;
+
+import java.util.Date;
+
+public class Contact {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String sex;
+
+    private Date birthday;
+
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
